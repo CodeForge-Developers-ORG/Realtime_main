5c25a372ba32ae5c419d281a9f18d865d29354d3
hamidrazaansari <hamdrazaanansari22@gmailcom>
2025-11-08 19:06:39 +0530

    new commit

--- .next/static/chunks/src_app_blog_page_tsx_39a3a2a8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_7fc8a6ff._.js",
-  "static/chunks/node_modules_e7614e89._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_career_page_tsx_39a3a2a8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_9ee6d130._.js",
-  "static/chunks/node_modules_a7137084._.js"
-],
-    source: "dynamic"
-});
